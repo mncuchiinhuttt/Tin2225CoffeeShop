5c5fb33c7fb56ffbcf77aaf2099930c9585e95f2
Vo Minh Long <vominhlongbentre@gmail.com>
2025-04-04 09:47:44 +0700

    okkokoko

--- users/models.py
+++ users/models.py
@@ -40,7 +40,7 @@ class Size(models.Model):
     
     menu_item = models.ForeignKey(MenuItem, related_name='sizes', on_delete=models.CASCADE)
     size = models.CharField(max_length=1, choices=SIZE_CHOICES)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=0)
     is_available = models.BooleanField(default=True)
 
     def __str__(self):
@@ -143,7 +143,7 @@ class OrderItem(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     size = models.ForeignKey(Size, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=0)
 
     def __str__(self):
         return f"{self.order.id} - {self.menu_item.name} ({self.size.get_size_display()})"
